268049a6a051832365ff3901e160289b3fcb3f43
Pamela Santamaría <71911127+pamshk@users.noreply.github.com>
2020-12-17 22:48:04 -0600

    Problem 1

---
+++ Times2.m
@@ -0,0 +1,9 @@
+function y = times2(x) % Do not edit this line.
+
+  % Modify the line below so that the output y is twice the incoming value x
+
+  y = x*2;
+
+  % After you modify the code, press the "Submit" button, and you're on your way.
+
+end % Do not edit this line.
